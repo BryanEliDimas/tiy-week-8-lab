dbe462afb907dd7894702dd356082fe565e32705
bryanelidimas <bryanelidimas@gmail.com>
2015-06-26 13:39:02 -0500

    Generate model and controller for Secrets

--- secrets-api/app/models/user.rb
+++ secrets-api/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, :password, presence: true
   validates :email, uniqueness: true
+  has_many :secrets
 end
--- secrets-api/config/routes.rb
+++ secrets-api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'secrets/index'
+
   post 'signup' => 'users#create', as: :users  # signup
   post 'signin' => 'sessions#create', as: :auth # signin
 
